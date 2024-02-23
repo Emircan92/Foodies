153d0c9f4a84591e5d263398b2e3ef83bffa7ae4
Emircan Kucukmotor <47698328+Emircan92@users.noreply.github.com>
2024-02-23 17:48:45 +0300

    Init commit

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>Not Found</h1>
+      <p>
+        Unfortunately, we could nto find the requested page or the resource.
+      </p>
+    </main>
+  );
+}
